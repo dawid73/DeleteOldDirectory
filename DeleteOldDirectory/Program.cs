c2d41c3ce18fef06d2d1a8bf033881fd543ab718
Dawid - work <dawid73@o2.pl>
2017-10-13 07:24:53 +0200

    EmptyDelayed

--- DeleteOldDirectory/Program.cs
+++ DeleteOldDirectory/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.IO;
 
@@ -13,37 +14,152 @@ namespace DeleteOldDirectory
         {
             foreach (System.IO.FileInfo file in directory.GetFiles())
             {
+
                 if (file.CreationTime < DateTime.Now.AddDays(-days))
                 {
-                    try
+                    if (file.ToString().StartsWith("@"))
                     {
-                        file.Delete();
-                        wpiszLog("Usunięto plik : " + file.ToString());
+
+                        EmptyDelayed(directory, CheckWidthDays(file.ToString()));
+
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        wpiszLog("Problem przy usuwaniu pliku: " + file.ToString() + " ||||| " + ex.ToString());
+
+                        try
+                        {
+                            file.Delete();
+                            wpiszLog("Usunięto plik : " + file.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            wpiszLog("Problem przy usuwaniu pliku: " + file.ToString() + " ||||| " + ex.ToString());
+                        }
                     }
-                    
                 }
             }
             foreach (System.IO.DirectoryInfo subDirectory in directory.GetDirectories())
             {
                 if (subDirectory.CreationTime < DateTime.Now.AddDays(-days))
                 {
-                    try
+                    if (subDirectory.ToString().StartsWith("@"))
                     {
-                        subDirectory.Delete(true);
-                        wpiszLog("Usunięto folder: " + subDirectory.ToString());
+
+                        EmptyDelayed(directory, CheckWidthDays(subDirectory.ToString()));
+
                     }
-                    catch (Exception ex)
+                    else
                     {
-                        wpiszLog("Problem przy usuwaniu foldera: " + subDirectory.ToString() + " ||||| " + ex.ToString());
+                        try
+                        {
+                            subDirectory.Delete(true);
+                            wpiszLog("Usunięto folder: " + subDirectory.ToString());
+                        }
+                        catch (Exception ex)
+                        {
+                            wpiszLog("Problem przy usuwaniu foldera: " + subDirectory.ToString() + " ||||| " + ex.ToString());
+                        }
                     }
-                   
+
                 }
             }
         }
+
+        public static void EmptyDelayed(this System.IO.DirectoryInfo directory, int days)
+        {
+            foreach (System.IO.FileInfo file in directory.GetFiles())
+            {
+                if (file.CreationTime < DateTime.Now.AddDays(-days) && file.ToString().StartsWith("@"))
+                {
+                    try
+                    {
+                        file.Delete();
+                        wpiszLog("Usunięto przełożony czasowo plik : " + file.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        wpiszLog("Problem przy usuwaniu przełożonego czasowo pliku: " + file.ToString() + " ||||| " + ex.ToString());
+                    }
+                }
+            }
+            foreach (System.IO.DirectoryInfo subDirectory in directory.GetDirectories())
+            {
+                if (subDirectory.CreationTime < DateTime.Now.AddDays(-days) && subDirectory.ToString().StartsWith("@"))
+                {
+                    try
+                    {
+                        subDirectory.Delete(true);
+                        wpiszLog("Usunięto przełożony czasowo folder: " + subDirectory.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        wpiszLog("Problem przy usuwaniu przełożonego czasowo foldera: " + subDirectory.ToString() + " ||||| " + ex.ToString());
+                    }
+                }
+            }
+        }
+
+        public static int CheckWidthDays(string nameFile)
+        {
+            string name = nameFile;
+
+            //Match match = Regex.Match(name, @"(@T[0-9]{1})@|@T([0-9]{2})@");
+
+            //Console.WriteLine(match.ToString());
+            //Console.ReadLine();
+
+            //string stringmatch = match.Value.ToString().Replace("@", "").Replace("T", "");
+            //if (stringmatch.Length > 0) {
+            //    int intOutWeek = int.Parse(stringmatch);
+            //    int intOutDays = intOutWeek * 7;
+            //    return intOutDays;
+            //}
+            //else
+            //{
+            //    int weeks = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["Weeks"]);
+            //    int days = weeks * 7;
+            //    return days;
+            //}
+
+            char[] tab = name.ToCharArray();
+            Console.WriteLine(tab[0]);
+            Console.WriteLine(tab[1]);
+            Console.WriteLine(tab[2]);
+            Console.WriteLine(tab[3]);
+            Console.WriteLine(tab[4]);
+            Console.WriteLine(tab[5]);
+            Console.WriteLine(tab[6]);
+            Console.ReadLine();
+
+            if ("@" == tab[3].ToString())
+            {
+                Console.WriteLine("jest małpa na tab[3]");
+                Console.ReadLine();
+
+                int a = int.Parse(tab[2].ToString());
+                return a * 7;
+            }
+            else if ("@" == tab[4].ToString())
+            {
+                Console.WriteLine("jest małpa na tab[4]");
+                Console.ReadLine();
+                int a = int.Parse(tab[2].ToString() + tab[3].ToString());
+                return a * 7;
+            }
+            else
+            {
+                Console.WriteLine("nie ma małpy");
+                Console.ReadLine();
+                return 14;
+            }
+
+
+
+
+
+        }
+
+
 
         static void Main(string[] args)
         {
@@ -53,25 +169,27 @@ namespace DeleteOldDirectory
             Console.WriteLine(FilePath);
 
             int weeks = Int32.Parse(System.Configuration.ConfigurationManager.AppSettings["Weeks"]);
-            int days = weeks * 7; 
+            int days = weeks * 7;
 
             System.IO.DirectoryInfo directory = new System.IO.DirectoryInfo(FilePath);
             Program.Empty(directory, days);
 
-            //Zatrzymanie programu
+
+
             //Console.ReadLine();
+
         }
 
         public static void wpiszLog(string log)
         {
             string tekstLoga;
             string logFilePatch = System.Configuration.ConfigurationManager.AppSettings["LogFilePath"];
-            
+
             tekstLoga = log;
             try
             {
                 File.AppendAllText(logFilePatch + "log.txt", DateTime.Now.ToString() + " :::: " + tekstLoga + Environment.NewLine);
-                
+
             }
             catch (Exception ex)
             {
@@ -79,10 +197,10 @@ namespace DeleteOldDirectory
                 Console.ReadLine();
                 throw;
             }
-            
-      
-        } 
 
-        
+
+        }
+
+
     }
 }
